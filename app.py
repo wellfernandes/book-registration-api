041e6ce6c6edb851a3429b83af55c7f80af347de
Welliton Fernandes Leal <wellfernandes@me.com>
2024-07-01 21:01:31 -0300

    fix: fixed database name

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_migrate import Migrate
 from db import db
+from models.book import Book
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/book'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/books'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
